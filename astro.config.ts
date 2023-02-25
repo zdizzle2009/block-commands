395272fe47b7e353a732bcbce474c3b52f3c7eea
Luke Bennett <hello@lukebennett.com.au>
2023-02-25 19:56:42 +1100

    Update to 2.0

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,9 @@
-import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
-// https://astro.build/config
 import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
+/** @see https://astro.build/config */
 export default defineConfig({
 	site: 'https://block-commands.netlify.app',
 	integrations: [
